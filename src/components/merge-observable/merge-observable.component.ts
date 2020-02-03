eec3a649eb25b04d8fce50597138c7be02f0472c
yorrran <pangyaran@hotmail.sg>
2020-02-03 13:43:55 +0800

    Mege obeservable with intervals and merge outer observables

--- src/components/merge-observable/merge-observable.component.ts
+++ src/components/merge-observable/merge-observable.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { fromEvent, of } from 'rxjs';
+import { Component, OnInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
+import { fromEvent, of, Subscription } from 'rxjs';
 import { mergeMap, delay, map } from 'rxjs/operators';
 
 @Component({
@@ -7,13 +7,29 @@ import { mergeMap, delay, map } from 'rxjs/operators';
   templateUrl: './merge-observable.component.html',
   styleUrls: ['./merge-observable.component.scss']
 })
-export class MergeObservableComponent implements OnInit {
-  clickedLocations: Array<string>=[];
+export class MergeObservableComponent implements OnInit, OnDestroy {
+  clickedLocations: Array<string> = [];
+  mergeSubscription: Subscription | undefined;
+
   @ViewChild('clickMerge1') clickMerge1: ElementRef | undefined;
   constructor() {}
 
-  ngOnInit() {
-    const clickEvent = of('x','y').pipe(mergeMap(e=>of(1,2).pipe(delay(500),map(num=>e+num)))).subscribe(item=>this.clickedLocations.push(item));
+  ngOnInit() {}
 
+  mergeObservables() {
+    this.mergeSubscription = of('x', 'y')
+      .pipe(
+        mergeMap(e =>
+          of(1, 2).pipe(
+            delay(500),
+            map(num => e + num)
+          )
+        )
+      )
+      .subscribe(item => this.clickedLocations.push(item));
+  }
+
+  ngOnDestroy(){
+    this.mergeSubscription.unsubscribe();
   }
 }
